8248b9ed240fe91f76a5262471bd68659200ca88
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-05-24 21:08:16 +0300

    Basic nahoy4

--- crates/nahoy2/src/main.rs
+++ crates/nahoy2/src/main.rs
@@ -11,13 +11,11 @@ use std::error::Error;
 
 // interacting with smart contracts
 
+#[allow(dead_code)]
 const NODE_ADDR: &str = "https://reth-ethereum.ithaca.xyz/rpc";
 const LOCAL_NODE_ADDR: &str = "127.0.0.1:8545";
 
-const ALICE: Address = address!("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 const ALICE_PK: &str = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-const BOB: Address = address!("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
-const CHARLIE: Address = address!("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
 
 const WETH: Address = address!("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
 
--- crates/nahoy4/src/main.rs
+++ crates/nahoy4/src/main.rs
@@ -1,3 +1,158 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::ops::{Add, Div, Mul, Sub};
+
+use alloy::primitives::utils::format_units;
+use alloy::{
+    primitives::{Address, U256, address},
+    uint,
+};
+
+pub static WETH_ADDR: Address = address!("C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+pub static DAI_ADDR: Address = address!("6B175474E89094C44Da98b954EedeAC495271d0F");
+
+#[derive(Debug)]
+pub struct UniV2Pair {
+    pub address: Address,
+    pub token0: Address,
+    pub token1: Address,
+    pub reserve0: U256,
+    pub reserve1: U256,
+}
+
+pub fn get_uniswap_pair() -> UniV2Pair {
+    UniV2Pair {
+        address: address!("A478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"),
+        token0: DAI_ADDR,
+        token1: WETH_ADDR,
+        reserve0: uint!(6227630995751221000110015_U256),
+        reserve1: uint!(2634810784674972449382_U256),
+    }
+}
+
+pub fn get_sushi_pair() -> UniV2Pair {
+    UniV2Pair {
+        address: address!("C3D03e4F041Fd4cD388c549Ee2A29a9E5075882f"),
+        token0: DAI_ADDR,
+        token1: WETH_ADDR,
+        reserve0: uint!(4314397529132715691120541_U256),
+        reserve1: uint!(1845242683965617816423_U256),
+    }
+}
+
+pub fn get_amount_out(reserve_in: U256, reserve_out: U256, amount_in: U256) -> U256 {
+    let amount_in_with_fee = amount_in * get_uniswappy_fee();
+    let numerator = amount_in_with_fee * reserve_out;
+    let denominator = reserve_in * U256::from(1000) + amount_in_with_fee;
+    numerator / denominator
+}
+
+pub fn get_amount_in(
+    reserves00: U256,
+    reserves01: U256,
+    is_weth0: bool,
+    reserves10: U256,
+    reserves11: U256,
+) -> U256 {
+    let numerator = get_numerator(reserves00, reserves01, is_weth0, reserves10, reserves11);
+    let denominator = get_denominator(reserves00, reserves01, is_weth0, reserves10, reserves11);
+
+    numerator * U256::from(1000) / denominator
+}
+
+fn sqrt(input: U256) -> U256 {
+    if input == U256::ZERO {
+        return U256::ZERO;
+    }
+
+    let mut z = (input + U256::from(1)) / U256::from(2);
+    let mut y = input;
+    while z < y {
+        y = z;
+        z = (input / z + z) / U256::from(2);
+    }
+    y
+}
+
+fn get_numerator(
+    reserves00: U256,
+    reserves01: U256,
+    is_weth0: bool,
+    reserves10: U256,
+    reserves11: U256,
+) -> U256 {
+    if is_weth0 {
+        let presqrt = get_uniswappy_fee()
+            .mul(get_uniswappy_fee())
+            .mul(reserves01)
+            .mul(reserves10)
+            .div(reserves11)
+            .div(reserves00);
+        sqrt(presqrt)
+            .sub(U256::from(1000))
+            .mul(reserves11)
+            .mul(reserves00)
+    } else {
+        let presqrt = get_uniswappy_fee()
+            .mul(get_uniswappy_fee())
+            .mul(reserves00)
+            .mul(reserves11)
+            .div(reserves10)
+            .div(reserves01);
+        (sqrt(presqrt))
+            .sub(U256::from(1000))
+            .mul(reserves10)
+            .mul(reserves01)
+    }
+}
+
+fn get_denominator(
+    reserves00: U256,
+    reserves01: U256,
+    is_weth0: bool,
+    reserves10: U256,
+    reserves11: U256,
+) -> U256 {
+    if is_weth0 {
+        get_uniswappy_fee()
+            .mul(reserves11)
+            .mul(U256::from(1000))
+            .add(get_uniswappy_fee().mul(get_uniswappy_fee()).mul(reserves01))
+    } else {
+        get_uniswappy_fee()
+            .mul(reserves10)
+            .mul(U256::from(1000))
+            .add(get_uniswappy_fee().mul(get_uniswappy_fee()).mul(reserves00))
+    }
+}
+
+fn get_uniswappy_fee() -> U256 {
+    U256::from(997)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let uniswap_pair = get_uniswap_pair();
+    let sushi_pair = get_sushi_pair();
+
+    let amount_in = get_amount_in(
+        uniswap_pair.reserve0,
+        uniswap_pair.reserve1,
+        false,
+        sushi_pair.reserve0,
+        sushi_pair.reserve1,
+    );
+
+    let dai_amount_out = get_amount_out(uniswap_pair.reserve1, uniswap_pair.reserve0, amount_in);
+    let weth_amount_out = get_amount_out(sushi_pair.reserve0, sushi_pair.reserve1, dai_amount_out);
+
+    if weth_amount_out < amount_in {
+        println!("no profit detected");
+        return Ok(());
+    }
+
+    let profit = weth_amount_out - amount_in;
+    println!("weth amount in: {}", format_units(amount_in, 18).unwrap());
+    println!("weth profit: {}", format_units(profit, 18).unwrap());
+
+    Ok(())
 }
